5fd6c633ac8a23ab0ee1e6ceed6dee14eabf8232
Dario Sechi <dario.sechi.rnd@gmail.com>
2023-01-12 21:13:17 +0100

    Add form-submit event to Form component

--- src/components/input/Form.tsx
+++ src/components/input/Form.tsx
@@ -1,4 +1,4 @@
 import { createBaseElementComponent, ElementComponent, FormProps } from '@queelag/react'
 import { FormElement } from '@queelag/web-components/elements/input/form.element.js'
 
-export const QForm: ElementComponent<FormElement, FormProps> = createBaseElementComponent('q-form', FormElement)
+export const QForm: ElementComponent<FormElement, FormProps> = createBaseElementComponent('q-form', FormElement, ['form-submit'])
