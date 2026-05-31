895ff51fd8de94c1dee9d8e8badf7c94c4d51b67
Dario Sechi <dario.sechi.rnd@gmail.com>
2026-05-31 18:15:51 +0200

    Upgrade deps and add prettier config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,7 @@
-import react from '@vitejs/plugin-react'
 import { join } from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [react()],
   resolve: {
     alias: {
       react: join(__dirname, 'node_modules/react')
